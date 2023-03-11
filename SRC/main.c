ecc934880b1df44396192d7509b434e0821b44f5
Sin_kider <2210486110@qq.com>
2023-03-11 20:29:08 +0800

    ex1-完成

---
+++ SRC/main.c
@@ -0,0 +1,11 @@
+#include "../INC/main.h"
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("Fuck world.");
+    puts("Fuck world.");
+    puts("Fuck world.");
+    puts("Fuck world.");
+
+    return 0;
+}
