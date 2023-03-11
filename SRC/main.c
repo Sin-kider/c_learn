062147e4bb5b46fff1b47a2bd5352656fa1ef135
Sin_kider <2210486110@qq.com>
2023-03-11 20:37:22 +0800

    ex3-完成

--- SRC/main.c
+++ SRC/main.c
@@ -1,11 +1,11 @@
 #include "../INC/main.h"
-int main(int argc, char *argv[])
+int main()
 {
-    puts("Hello world.");
-    puts("Fuck world.");
-    puts("Fuck world.");
-    puts("Fuck world.");
-    puts("Fuck world.");
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
 
     return 0;
 }
