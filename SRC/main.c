c115a791030a89753ba4d52b6bc056450585c2e6
Sin_kider <2210486110@qq.com>
2023-03-12 17:02:07 +0800

    ex10-完成

--- SRC/main.c
+++ SRC/main.c
@@ -1,54 +1,24 @@
 #include "../INC/main.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    int numbers[4] = {'a', 'b', 'c', 'd'};
-    char name[4] = {48, 49, 50, 51};
+int main(int argc, char *argv[]) {
+  int i = 0;
 
-    // first, print them out raw
-    printf("numbers: %c %c %c %c\n",
-            numbers[0], numbers[1],
-            numbers[2], numbers[3]);
+  // go through each string in argv
+  // why am I skipping argv[0]?
+  
 
-    printf("name each: %d %d %d %d\n",
-            name[0], name[1],
-            name[2], name[3]);
+  // let's make our own array of strings
+  char *states[] = {"California", "Oregon", "Washington", "Texas", NULL};
+  int num_states = 5;
+  argv[1] = states[1];
+  for (i = 1; i < argc; i++) {
+    printf("arg %d: %s\n", i, argv[i]);
+  }
+  states[2] = argv[2];
+  for (i = 0; i < num_states; i++) {
+    printf("state %d: %s\n", i, states[i]);
+  }
 
-    printf("name: %s\n", name);
-
-    // setup the numbers
-    numbers[0] = 1;
-    numbers[1] = 2;
-    numbers[2] = 3;
-    numbers[3] = 4;
-
-    // setup the name
-    name[0] = 'Z';
-    name[1] = 'e';
-    name[2] = 'd';
-    name[3] = '\0';
-
-    // then print them out initialized
-    printf("numbers: %d %d %d %d\n",
-            numbers[0], numbers[1],
-            numbers[2], numbers[3]);
-
-    printf("name each: %c %c %c %c\n",
-            name[0], name[1],
-            name[2], name[3]);
-
-    // print the name like a string
-    printf("name: %s\n", name);
-
-    // another way to use name
-    // char *another = "Zed";
-    char *another = name;
-
-    printf("another: %s\n", another);
-
-    printf("another each: %c %c %c %c\n",
-            another[0], another[1],
-            another[2], another[3]);
-
-    return 0;
+  return 0;
 }
