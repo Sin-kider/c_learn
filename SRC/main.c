6e1bb9efe8e57055f59d5e487ba000783590979f
Sin_kider <2210486110@qq.com>
2023-03-12 17:56:00 +0800

    ex14-完成

--- SRC/main.c
+++ SRC/main.c
@@ -1,59 +1,35 @@
 #include "../INC/main.h"
 
-char lower(char a);
+// forward declarations
+void print_letters(char arg[]);
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("ERROR: You need one argument.\n");
-    // this is how you abort a program
-    for (int j = 0;j < argc;j++) {
-      puts(argv[j]);
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+
+    for(i = 0; i < argc; i++) {
+        print_letters(argv[i]);
     }
-    return 1;
-  }
-  int i = 0;
-  for (char letter; argv[1][i] != '\0'; i++) {
-    letter = argv[1][i];
-
-    switch (lower(letter)) {
-    case 'a':
-      printf("%d: 'A'\n", i);
-      break;
-
-    case 'e':
-      printf("%d: 'E'\n", i);
-      break;
-
-    case 'i':
-      printf("%d: 'I'\n", i);
-      break;
-
-    case 'o':
-      printf("%d: 'O'\n", i);
-      break;
-
-    case 'u':
-      printf("%d: 'U'\n", i);
-      break;
-
-    case 'y':
-      if (i > 2) {
-        // it's only sometimes Y
-        printf("%d: 'Y'\n", i);break;
-      }
-      
-
-    default:
-      printf("%d: %c is not a vowel\n", i, letter);
-    }
-  }
-
-  return 0;
 }
 
-char lower(char a) {
-  if (a <= 'Z' && a >= 'A') {
-    return a + ('a' - 'A');
-  }
-  return a;
+void print_letters(char arg[])
+{
+    int i = 0;
+
+    for(i = 0; i < strlen(arg); i++) {
+        char ch = arg[i];
+
+        if(isalpha(ch) || isblank(ch)) {
+            printf("'%c' == %d ", ch, ch);
+        }
+    }
+
+    printf("\n");
+}
+
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+    return 0;
 }
