1876d997fe32087d2dbb50d6eec1e539cc6dcd65
Sin_kider <2210486110@qq.com>
2023-03-12 17:46:48 +0800

    ex13-完成

--- SRC/main.c
+++ SRC/main.c
@@ -1,20 +1,59 @@
 #include "../INC/main.h"
 
+char lower(char a);
+
 int main(int argc, char *argv[]) {
-  int i = 0;
-
-  if (argc == 1) {
-    printf("You only have two argument. You suck.\n");
-  } else if (argc > 1 && argc < 4) {
-    printf("Here's your arguments:\n");
-
-    for (i = 0; i < argc; i++) {
-      printf("%s ", argv[i]);
+  if (argc != 2) {
+    printf("ERROR: You need one argument.\n");
+    // this is how you abort a program
+    for (int j = 0;j < argc;j++) {
+      puts(argv[j]);
     }
-    printf("\n");
-  } else {
-    printf("You have too many arguments. You suck.\n");
+    return 1;
+  }
+  int i = 0;
+  for (char letter; argv[1][i] != '\0'; i++) {
+    letter = argv[1][i];
+
+    switch (lower(letter)) {
+    case 'a':
+      printf("%d: 'A'\n", i);
+      break;
+
+    case 'e':
+      printf("%d: 'E'\n", i);
+      break;
+
+    case 'i':
+      printf("%d: 'I'\n", i);
+      break;
+
+    case 'o':
+      printf("%d: 'O'\n", i);
+      break;
+
+    case 'u':
+      printf("%d: 'U'\n", i);
+      break;
+
+    case 'y':
+      if (i > 2) {
+        // it's only sometimes Y
+        printf("%d: 'Y'\n", i);break;
+      }
+      
+
+    default:
+      printf("%d: %c is not a vowel\n", i, letter);
+    }
   }
 
   return 0;
+}
+
+char lower(char a) {
+  if (a <= 'Z' && a >= 'A') {
+    return a + ('a' - 'A');
+  }
+  return a;
 }
