8cd47abb6389ebb419dfc3da20f6feb11db8e6e6
Sin_kider <2210486110@qq.com>
2023-03-12 17:15:43 +0800

    ex11-完成

--- SRC/main.c
+++ SRC/main.c
@@ -1,22 +1,25 @@
 #include "../INC/main.h"
 
 int main(int argc, char *argv[]) {
-  int i = 0;
-
   // go through each string in argv
-  // why am I skipping argv[0]?
-  
+
+  int i = argc;
+  while (i > 0) {
+    printf("arg %d: %s\n", i - 1, argv[i - 1]);
+    --i;
+  }
 
   // let's make our own array of strings
-  char *states[] = {"California", "Oregon", "Washington", "Texas", NULL};
-  int num_states = 5;
-  argv[1] = states[1];
-  for (i = 1; i < argc; i++) {
-    printf("arg %d: %s\n", i, argv[i]);
-  }
-  states[2] = argv[2];
-  for (i = 0; i < num_states; i++) {
+  char *states[] = {"California", "Oregon", "Washington", "Texas"};
+
+  int num_states = 4;
+  i = 0; // watch for this
+  while (i < num_states) {
+    if (i < argc) {
+      states[i] = argv[i];
+    }
     printf("state %d: %s\n", i, states[i]);
+    i++;
   }
 
   return 0;
