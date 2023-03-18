3271aa1eecabe1b11d552e14d9fb5599dbb2ebd5
Sin_kider <2210486110@qq.com>
2023-03-18 20:49:35 +0800

    ex42-完成

---
+++ INC/stack.h
@@ -0,0 +1,21 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include "list.h"
+
+typedef List Stack;
+
+#define Stack_create List_create
+#define Stack_destroy List_destroy
+#define Stack_clear List_clear
+#define Stack_clear_destroy List_clear_destroy
+
+#define Stack_push List_push
+#define Stack_pop List_pop
+#define Stack_peek List_last
+
+#define Stack_count List_count
+
+#define STACK_FOREACH(S, V) LIST_FOREACH(S, last, prev, V)
+
+#endif
